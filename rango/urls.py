51f382ecda77730a709fab7e164fde8960ad18e9
ratnam <ratnam1026@gmail.com>
2017-06-01 16:25:08 +0530

    firstcommit

---
+++ rango/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
+from rango import views
+
+
+urlpatterns = [
+url(r'^$', views.index, name='index')
+#url(r'^rango/', include('rango.urls')),
+]
